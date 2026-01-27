8ca941b12131c9e41993d41b511b0b4d271884a3
hitchcliff <hitchcliff1@gmail.com>
2026-01-28 06:11:59 +0800

    password change

--- admin/reset-password.php
+++ admin/reset-password.php
@@ -1,4 +1,51 @@
-<?php include("./components/header.php") ?>
+<?php
+
+include("./components/header.php");
+
+$email = $_REQUEST["email"];
+$token = $_REQUEST["token"];
+$sql = "SELECT * FROM users WHERE email='$email' AND token='$token'";
+$query = $pdo->query($sql);
+
+$loginUrl = ADMIN_URL . "login.php";
+
+if ($query->rowCount() > 0) {
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+} else {
+    header("Location: $loginUrl");
+}
+
+if (isset($_POST["form_forget_password"])) {
+    try {
+        $password = $_POST["password"];
+        $confirm_password = $_POST["confirm_password"];
+
+        if (empty($password) || empty($confirm_password)) {
+            throw new Exception("Passwords cannot be empty");
+        }
+
+        if ($password != $confirm_password) {
+            throw new Exception("Password doesn't match");
+        }
+
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $user_id = $row['id'];
+        $sql2 = "UPDATE users SET password='$hashed_password' WHERE id='$user_id'";
+        $query2 = $pdo->query($sql2);
+
+        if ($query2->rowCount() > 0) {
+            $success_message = "Password changed successfully";
+        }
+
+    } catch (\Throwable $th) {
+        $error_message = $th->getMessage();
+    }
+
+}
+
+
+?>
 
 <section class="section">
     <div class="container container-login">
@@ -9,17 +56,34 @@
                         <h4 class="text-center">Reset Password</h4>
                     </div>
                     <div class="card-body card-body-auth">
-                        <form method="POST" action="">
+
+
+                        <?php if (isset($error_message)) { ?>
+                            <span class="text-danger d-flex align-items-center gap-1 mb-2">
+                                <span class="material-icons">error</span>
+                                <?= $error_message; ?>
+                            </span>
+                        <?php }
+                        if (isset($success_message)) { ?>
+                            <span class="text-success d-flex align-items-center gap-1 mb-2">
+                                <span class="material-icons">check</span>
+                                <?= $success_message; ?>
+                            </span>
+                        <?php } ?>
+
+
+                        <form method="POST">
                             <div class="form-group">
-                                <input type="password" class="form-control" name="" placeholder="Password" value=""
-                                    autofocus>
+                                <input type="password" class="form-control" name="password" placeholder="Password"
+                                    value="" autofocus>
                             </div>
                             <div class="form-group">
-                                <input type="password" class="form-control" name="" placeholder="Retype Password"
-                                    value="">
+                                <input type="password" class="form-control" name="confirm_password"
+                                    placeholder="Retype Password" value="">
                             </div>
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-lg w_100_p">
+                                <button type="submit" class="btn btn-primary btn-lg w_100_p"
+                                    name="form_forget_password">
                                     Submit
                                 </button>
                             </div>
