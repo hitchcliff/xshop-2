5b61e3d2ff3ac986f90dc0c91685892e8ec681d1
hitchcliff <hitchcliff1@gmail.com>
2026-03-10 19:59:46 +0800

    verify user, assign status and new token

--- register-verify.php
+++ register-verify.php
@@ -2,13 +2,40 @@
 
 include('./templates/header.php');
 
+$redirectUrl = BASE_URL . 'account-signin.php';
+
 if (!($_GET['email']) || !($_GET['token'])) {
 
-    $redirectUrl = BASE_URL . 'account-signin.php';
 
     header("Location: $redirectUrl");
     exit();
 }
+
+// check for the user
+$email = $_GET['email'];
+$token = $_GET['token'];
+$sql = "SELECT * FROM customers WHERE email='$email' AND token='$token'";
+$query = $pdo->query($sql);
+
+// cannot find
+if ($query->rowCount() == 0) {
+    header("Location: $redirectUrl");
+    exit();
+}
+
+$newToken = bin2hex(random_bytes(12));
+
+// update the status and assign new token
+$sql = "UPDATE customers SET status='active', token='$newToken' WHERE email='$email' AND token='$token'";
+$query = $pdo->query($sql);
+
+if ($query->rowCount() > 0) {
+    $success_message = "You account has been activated! You may login now";
+    $_SESSION['success_message'] = $success_message;
+    header("Location: $redirectUrl");
+    exit();
+}
+
 ?>
 
 
