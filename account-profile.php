6bbffdd309a49bbe893067428a6f02021c35ebca
hitchcliff <hitchcliff1@gmail.com>
2026-05-06 02:33:14 +0800

    layout

--- account-profile.php
+++ account-profile.php
@@ -1,53 +1,29 @@
-<?php include('./templates/header.php') ?>
+<?php
+include('./templates/header.php');
+
+$_SESSION['page_title'] = "Profile Info";
+
+if (isset($_SESSION['form_profile'])) {
+
+
+
+
+
+
+}
+
+?>
+
+
+<!-- Page Title-->
+<?php include('./templates/page-title.php'); ?>
+
 
 <div class="container pb-5 mb-2 mb-md-4">
   <div class="row">
     <!-- Sidebar-->
-    <aside class="col-lg-4 pt-4 pt-lg-0 pe-xl-5">
-      <div class="bg-white rounded-3 shadow-lg pt-1 mb-5 mb-lg-0">
-        <div class="d-md-flex justify-content-between align-items-center text-center text-md-start p-4">
-          <div class="d-md-flex align-items-center">
-            <div class="img-thumbnail rounded-circle position-relative flex-shrink-0 mx-auto mb-2 mx-md-0 mb-md-0"
-              style="width: 6.375rem;"><span class="badge bg-warning position-absolute end-0 mt-n2"
-                data-bs-toggle="tooltip" title="Reward points">384</span><img class="rounded-circle"
-                src="<?php BASE_URL ?>dist-front/img/shop/account/avatar.jpg" alt="Susan Gardner"></div>
-            <div class="ps-md-3">
-              <h3 class="fs-base mb-0">Susan Gardner</h3><span class="text-accent fs-sm">s.gardner@example.com</span>
-            </div>
-          </div><a class="btn btn-primary d-lg-none mb-2 mt-3 mt-md-0" href="#account-menu" data-bs-toggle="collapse"
-            aria-expanded="false"><i class="ci-menu me-2"></i>Account menu</a>
-        </div>
-        <div class="d-lg-block collapse" id="account-menu">
-          <div class="bg-secondary px-4 py-3">
-            <h3 class="fs-sm mb-0 text-muted">Dashboard</h3>
-          </div>
-          <ul class="list-unstyled mb-0">
-            <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3"
-                href="account-orders.html"><i class="ci-bag opacity-60 me-2"></i>Orders<span
-                  class="fs-sm text-muted ms-auto">1</span></a></li>
-            <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3"
-                href="account-wishlist.html"><i class="ci-heart opacity-60 me-2"></i>Wishlist<span
-                  class="fs-sm text-muted ms-auto">3</span></a></li>
-            <li class="mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3"
-                href="account-tickets.html"><i class="ci-help opacity-60 me-2"></i>Support tickets<span
-                  class="fs-sm text-muted ms-auto">1</span></a></li>
-          </ul>
-          <div class="bg-secondary px-4 py-3">
-            <h3 class="fs-sm mb-0 text-muted">Account settings</h3>
-          </div>
-          <ul class="list-unstyled mb-0">
-            <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3 active"
-                href="account-profile.html"><i class="ci-user opacity-60 me-2"></i>Profile info</a></li>
-            <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3"
-                href="account-address.html"><i class="ci-location opacity-60 me-2"></i>Addresses</a></li>
-            <li class="mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3"
-                href="account-payment.html"><i class="ci-card opacity-60 me-2"></i>Payment methods</a></li>
-            <li class="d-lg-none border-top mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3"
-                href="account-signin.html"><i class="ci-sign-out opacity-60 me-2"></i>Sign out</a></li>
-          </ul>
-        </div>
-      </div>
-    </aside>
+    <?php include('./templates/sidebar.php'); ?>
+
     <!-- Content  -->
     <section class="col-lg-8">
       <!-- Toolbar-->
@@ -70,24 +46,28 @@
         <div class="row gx-4 gy-3">
           <div class="col-sm-6">
             <label class="form-label" for="account-fn">First Name</label>
-            <input class="form-control" type="text" id="account-fn" value="Susan">
+            <input class="form-control" type="text" id="account-fn"
+              value="<?php echo $_SESSION['customer']['first_name'] ?>">
           </div>
           <div class="col-sm-6">
             <label class="form-label" for="account-ln">Last Name</label>
-            <input class="form-control" type="text" id="account-ln" value="Gardner">
+            <input class="form-control" type="text" id="account-ln"
+              value="<?php echo $_SESSION['customer']['last_name'] ?>">
           </div>
           <div class="col-sm-6">
             <label class="form-label" for="account-email">Email Address</label>
-            <input class="form-control" type="email" id="account-email" value="s.gardner@example.com" disabled>
+            <input class="form-control" type="email" id="account-email"
+              value="<?php echo $_SESSION['customer']['email'] ?>" disabled>
           </div>
           <div class="col-sm-6">
             <label class="form-label" for="account-phone">Phone Number</label>
-            <input class="form-control" type="text" id="account-phone" value="+7 (805) 348 95 72" required>
+            <input class="form-control" type="text" id="account-phone"
+              value="<?php echo $_SESSION['customer']['phone_number'] ? $_SESSION['customer']['phone_number'] : '' ?>">
           </div>
           <div class="col-sm-6">
             <label class="form-label" for="account-pass">New Password</label>
             <div class="password-toggle">
-              <input class="form-control" type="password" id="account-pass">
+              <input class="form-control" name="password" type="password" id="account-pass">
               <label class="password-toggle-btn" aria-label="Show/hide password">
                 <input class="password-toggle-check" type="checkbox"><span class="password-toggle-indicator"></span>
               </label>
@@ -96,22 +76,22 @@
           <div class="col-sm-6">
             <label class="form-label" for="account-confirm-pass">Confirm Password</label>
             <div class="password-toggle">
-              <input class="form-control" type="password" id="account-confirm-pass">
+              <input class="form-control" name="confirm_password" type="password" id="account-confirm-pass">
               <label class="password-toggle-btn" aria-label="Show/hide password">
                 <input class="password-toggle-check" type="checkbox"><span class="password-toggle-indicator"></span>
               </label>
             </div>
           </div>
-          <!-- <div class="col-12">
+          <div class="col-12">
             <hr class="mt-2 mb-3">
             <div class="d-flex flex-wrap justify-content-between align-items-center">
-              <div class="form-check">
+              <!-- <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="subscribe_me" checked>
                 <label class="form-check-label" for="subscribe_me">Subscribe me to Newsletter</label>
-              </div>
-              <button class="btn btn-primary mt-3 mt-sm-0" type="button">Update profile</button>
+              </div> -->
+              <button name="form_profile" class="btn btn-primary mt-3 mt-sm-0" type="button">Update profile</button>
             </div>
-          </div> -->
+          </div>
         </div>
       </form>
     </section>
---
+++ templates/page-title.php
@@ -0,0 +1,7 @@
+<div class="page-title-overlap bg-dark pt-4">
+    <div class="container d-lg-flex justify-content-between py-2 py-lg-3">
+        <div class="order-lg-1 pe-lg-4 text-center text-lg-start">
+            <h1 class="h3 text-light mb-0"><?php echo $_SESSION['page_title'] ?></h1>
+        </div>
+    </div>
+</div>
